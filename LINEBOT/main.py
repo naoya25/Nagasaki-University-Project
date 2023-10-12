79f9069ae322c82ac21ea56718b06f75ad8aa346
naoya25 <onaoya2002@gmail.com>
2023-10-12 16:41:16 +0900

    first commit

---
+++ LINEBOT/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, abort
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+import os
+
+app = Flask(__name__)
+
+configuration = Configuration(access_token=os.environ['TOKEN'])
+handler = WebhookHandler(os.environ['SECRET'])
+
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info('Request body: ' + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info('Invalid signature. Please check your channel access token/channel secret.')
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
+
+if __name__ == '__main__':
+    app.run()
