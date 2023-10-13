38adff8b16ec7cc30527a7c20037dbfaac4f6900
naoya25 <onaoya2002@gmail.com>
2023-10-13 14:30:36 +0900

    fix

--- app.py
+++ app.py
@@ -7,38 +7,38 @@ from model import create_image
 
 app = Flask(__name__)
 
-# line_bot_api = LineBotApi(os.getenv('ACCESS_TOKEN'))
-# handler = WebhookHandler(os.getenv('SECRET'))
+line_bot_api = LineBotApi(os.getenv('ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('SECRET'))
 
-# @app.route('/callback', methods=['POST'])
-# def callback():
-#     signature = request.headers['X-Line-Signature']
-#     body = request.get_data(as_text=True)
-#     app.logger.info('Request body: ' + body)
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info('Request body: ' + body)
 
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-#     return 'OK'
+    return 'OK'
 
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     text = event.message.text
-#     # if create_image(text):
-#     messages = ImageSendMessage(
-#         original_content_url='./image.png',
-#         preview_image_url='./image.png'
-#     )
-#     line_bot_api.reply_message(
-#         event.reply_token, messages
-#     )
-#     # else:
-#     #     line_bot_api.reply_message(
-#     #         event.reply_token, TextSendMessage(text='画像生成に失敗しました')
-#     #     )
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text = event.message.text
+    if create_image(text):
+        messages = ImageSendMessage(
+            original_content_url='./static/image.png',
+            preview_image_url='./static/image.png'
+        )
+        line_bot_api.reply_message(
+            event.reply_token, messages
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text='画像生成に失敗しました')
+        )
 
 @app.route('/')
 def index():
