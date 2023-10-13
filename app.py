87c56fc15d973e44556e4378d76c51f6e98bf789
naoya25 <onaoya2002@gmail.com>
2023-10-13 10:26:02 +0900

    add
    line image send

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 import os
 
 app = Flask(__name__)
@@ -25,9 +25,17 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    messages = make_image_message()
     line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=event.message.text)
+        event.reply_token, messages
     )
+
+def make_image_message():
+    messages = ImageSendMessage(
+        original_content_url="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.668xw:1.00xh;0.119xw,0&resize=1200:*",
+        preview_image_url="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.668xw:1.00xh;0.119xw,0&resize=1200:*"
+    )
+    return messages
 
 
 if __name__ == '__main__':
